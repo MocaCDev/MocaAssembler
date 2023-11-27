b0798f8382d5036ae90566c1349efcaef301a0ed
MocaCDev <t.targetyt@gmail.com>
2023-11-26 21:47:38 -0600

    Updated assert in main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@ using namespace AssemblerRun;
 
 int main(int args, char *argv[])
 {
-	moca_assembler_assert(args > 1, EXIT_FAILURE, "Expected assembly file as input.\n")
+	moca_assembler_assert(
+		args > 1, 
+		EXIT_FAILURE, 
+		"Expected assembly file as input.\n")
 	
 	asm_run assembler_run(argv[1]);
 
